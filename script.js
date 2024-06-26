8d076f5e5253d25fefe6e1a2924f85e127f3d40f
Alexander-Agu <theonlyagu@gmail.com>
2024-06-26 08:48:08 +0200

    Fixed my button functionality now the can display their values

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Their value will be appended to an array
 // Wich i will use detrime the winner by using switch statement
 
+
+// Constant Variables
 const player = document.getElementById("player");
 const buttonOne = document.getElementById("btn-1");
 const buttonTwo = document.getElementById("btn-2");
@@ -14,24 +16,176 @@ const buttonSeven = document.getElementById("btn-7");
 const buttonAight = document.getElementById("btn-8");
 const buttonNine = document.getElementById("btn-9");
 
-let gameBoard = []
-let firstRow = [1, 2 ,3];
-let seconRow = [];
-let thirdRow = [];
-let value = player.value;
 
 
-buttonOne.onclick = ()=>{
-    if(value.toUpperCase() === "X"){
-        firstRow.splice(0, 1, "X")
-        value = "O"
+// 
+let gameBoard = [];
+// Will be replaced by the values of x and o when button is clicked and put together in the game board
+let firstRow = [0,1,2];
+let secondRow = [0,1,2];
+let thirdRow = [0,1,2];
+
+
+
+
+// Handles the click event for the first row
+function boardOne(){
+    buttonOneClicked = true;
+    value = player.value;
+    if(value === 'x'){
         buttonOne.textContent = "X";
+        player.value = "o"
+        firstRow.splice(0,1, "X")
+        buttonOne.onclick = null;
     }
-    else if(value.toUpperCase() === "O"){
-        firstRow.splice(0, 1, input)
-        player.value = "X"
-        buttonOne.textContent = "X";
+    else if(value === 'o'){
+        buttonOne.textContent = "O";
+        player.value = "x"
+        firstRow.splice(0,1, "O")
+        buttonOne.onclick = null;
     }
+    return firstRow
+};
+function boardTwo(){
+    value = player.value;
+    if(value === 'x'){
+        buttonTwo.textContent = "X";
+        player.value = "o"
+        firstRow.splice(1,1, "X")
+        buttonTwo.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonTwo.textContent = "O";
+        player.value = "x"
+        firstRow.splice(1,1, "O")
+        buttonTwo.onclick = null;
+    }
+    return firstRow
+};
+function boardThree(){
+    value = player.value;
+    if(value === 'x'){
+        buttonThree.textContent = "X";
+        player.value = "o"
+        firstRow.splice(2,1, "X")
+        buttonThree.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonThree.textContent = "O";
+        player.value = "x"
+        firstRow.splice(2,1, "O")
+        buttonThree.onclick = null;
+    }
+    return firstRow
 };
 
-console.log(value)
+// Handles click event for the second row
+function boardFour(){
+    value = player.value;
+    if(value === 'x'){
+        buttonFour.textContent = "X";
+        player.value = "o"
+        secondRow.splice(0,1, "X")
+        buttonFour.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonFour.textContent = "O";
+        player.value = "x"
+        secondRow.splice(0,1, "O")
+        buttonFour.onclick = null;
+    }
+    return secondRow
+};
+function boardFive(){
+    value = player.value;
+    if(value === 'x'){
+        buttonFive.textContent = "X";
+        player.value = "o"
+        secondRow.splice(1,1, "X")
+        buttonFive.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonFive.textContent = "O";
+        player.value = "x"
+        secondRow.splice(1,1, "O")
+        buttonFive.onclick = null;
+    }
+    return secondRow
+};
+function boardSix(){
+    value = player.value;
+    if(value === 'x'){
+        buttonSix.textContent = "X";
+        player.value = "o"
+        secondRow.splice(2,1, "X")
+        buttonSix.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonSix.textContent = "O";
+        player.value = "x"
+        secondRow.splice(2,1, "O")
+        buttonSix.onclick = null;
+    }
+    return secondRow
+};
+
+// Handles click Event for the third row
+function boardSeven(){
+    value = player.value;
+    if(value === 'x'){
+        buttonSeven.textContent = "X";
+        player.value = "o"
+        thirdRow.splice(0,1, "X")
+        buttonSeven.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonSeven.textContent = "O";
+        player.value = "x"
+        thirdRow.splice(0,1, "O")
+        buttonSeven.onclick = null;
+    }
+    return thirdRow
+};
+function boardAight(){
+    value = player.value;
+    if(value === 'x'){
+        buttonAight.textContent = "X";
+        player.value = "o"
+        thirdRow.splice(1,1, "X")
+        buttonAight.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonAight.textContent = "O";
+        player.value = "x"
+        thirdRow.splice(1,1, "O")
+        buttonAight.onclick = null;
+    }
+    return thirdRow
+};
+function boardNine(){
+    value = player.value;
+    if(value === 'x'){
+        buttonNine.textContent = "X";
+        player.value = "o"
+        thirdRow.splice(2,1, "X")
+        buttonNine.onclick = null;
+    }
+    else if(value === 'o'){
+        buttonNine.textContent = "O";
+        player.value = "x"
+        thirdRow.splice(2,1, "O")
+        buttonNine.onclick = null;
+    }
+    return thirdRow
+};
+
+function checkWinner(){
+    firstRow = [boardOne()];
+    secondRow = [boardFour()];
+    thirdRow = [boardSeven()];
+    gameBoard = [firstRow, secondRow, thirdRow];
+    console.log(gameBoard)
+
+
+
+};
